bfda44f9b49eebee9aeb42a34899094c20e29fbc
Adriel Rosa <minilord002@gmail.com>
2025-02-13 10:50:29 -0300

    fix failure

--- src/script.js
+++ src/script.js
@@ -38,7 +38,7 @@ document.querySelector('button[type="submit"]').addEventListener(
       return showAndHideErrorMessage();
     }
     if (isPhoneRequired && !phoneField.value) {
-      // return showAndHideErrorMessage()
+      return showAndHideErrorMessage();
     }
     if (
       !emailField.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
